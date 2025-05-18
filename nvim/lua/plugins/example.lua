429595d93d13e08f8c12c692b951052c172031a1
codyferd <lucarappferdinand@gmail.com>
2025-05-17 20:38:27 -0400

    edited lazyvum

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -175,21 +175,11 @@ return {
       }
     end,
   },
-
   -- use mini.starter instead of alpha
   { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
-
-  {
-    "olimorris/codecompanion.nvim",
-    opts = {},
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
   -- add any tools you want to have installed below
   {
     "williamboman/mason.nvim",
